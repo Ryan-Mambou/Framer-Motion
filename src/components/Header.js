63e3403d9803bbde1ed04e4c3b84510bbc5a8997
Ryan Djemtchemeu <ryanmambou@gmail.com>
2024-10-06 12:29:24 +0100

    feat: draggable header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,11 @@ const pathVariants = {
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <motion.div
+        drag
+        className="logo"
+        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+      >
         <motion.svg
           variants={svgVariants}
           initial="initial"
@@ -38,7 +42,7 @@ const Header = () => {
             d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
           />
         </motion.svg>
-      </div>
+      </motion.div>
       <div className="title">
         <motion.h1
           initial={{ y: 250 }}
